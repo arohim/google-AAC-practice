f9927d158892b3597ff77d406fe0386823372d6d
Abdulrohim 'Him' Sama <abdulrohim.s@kaidee.com>
2019-07-31 00:20:27 +0700

    added recyclerview adapter

--- app/src/main/java/com/example/aad1/TodoListActivity.kt
+++ app/src/main/java/com/example/aad1/TodoListActivity.kt
@@ -10,6 +10,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class TodoListActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/aad1/adapter/TodoListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.aad1.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatCheckBox;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.aad1.R;
+import com.example.aad1.customview.PriorityStarImageView;
+
+public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.TodoListAdapterViewHolder> {
+
+    @NonNull
+    @Override
+    public TodoListAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_todo_list, parent, false);
+        TodoListAdapterViewHolder viewHolder = new TodoListAdapterViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodoListAdapterViewHolder holder, int position) {
+        holder.cbTodoDescription.setText("xxx");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class TodoListAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final AppCompatCheckBox cbTodoDescription;
+        final TextView tvTodoDueDate;
+        final TextView tvTodoPriority;
+        final PriorityStarImageView ivTodoPriorityStar;
+        final ConstraintLayout clTodoListItem;
+
+        public TodoListAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cbTodoDescription = itemView.findViewById(R.id.cb_todo_description);
+            tvTodoDueDate = itemView.findViewById(R.id.tv_todo_due_date);
+            tvTodoPriority = itemView.findViewById(R.id.tv_todo_priority);
+            ivTodoPriorityStar = itemView.findViewById(R.id.iv_todo_priority_star);
+            clTodoListItem = (ConstraintLayout) itemView;
+            itemView.setOnClickListener(this);
+            cbTodoDescription.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
