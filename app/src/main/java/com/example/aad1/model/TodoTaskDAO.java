490fb23a009144c67094fd59d6e54492534253b0
Abdulrohim 'Him' Sama <abdulrohim.s@kaidee.com>
2019-08-03 16:07:33 +0700

    changed DAO to use LiveData instead

--- app/src/main/java/com/example/aad1/model/TodoTaskDAO.java
+++ app/src/main/java/com/example/aad1/model/TodoTaskDAO.java
@@ -1,12 +1,11 @@
 package com.example.aad1.model;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
-
-import java.util.List;
 
 @Dao
 public interface TodoTaskDAO {
@@ -21,13 +20,13 @@ public interface TodoTaskDAO {
     void update(TodoTask... todoTask);
 
     @Query("SELECT * FROM TodoTask")
-    List<TodoTask> loadTodoTasks();
+    LiveData<TodoTask> loadTodoTasks();
 
     @Query("SELECT * FROM TodoTask WHERE completed = :completed")
-    List<TodoTask> loadTodoTasksByCompletedStatus(int completed);
+    LiveData<TodoTask> loadTodoTasksByCompletedStatus(int completed);
 
 
     @Query("SELECT * FROM TodoTask WHERE priority = :priority")
-    List<TodoTask> loadTodoTasksByPriority(int priority);
-    
+    LiveData<TodoTask> loadTodoTasksByPriority(int priority);
+
 }
